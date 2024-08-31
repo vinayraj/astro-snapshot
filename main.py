9fdae4f60a18b85a53c038308d66b0bca25c56fb
vinayraj <vinayraj.hampapur@gmail.com>
2024-08-31 11:41:12 -0700

    Create main.py
    Steps to create a functioning GUI interaction tool

---
+++ main.py
@@ -0,0 +1,19 @@
+# Step 1
+# Grab JHora executable at: https://www.vedicastrologer.org/jh/#download
+
+# Step 2
+# Launch the executable with the package subprocess: https://docs.python.org/3/library/subprocess.html
+
+# Step 3
+#  Interact with GUI element to export produced chart as a txt file
+# Use any of the following libraries: pyautogui,PyWinAuto
+
+# Step 4
+# Interact with GUI element to export chart as a png
+# Use the same libraries as above
+
+# Step 5
+# Set up post-processing module to extract pertinent elements from the text
+
+# Step 6
+# Wrap python executable as a service
